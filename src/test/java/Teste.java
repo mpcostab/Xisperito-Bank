415b89d180ddf749a8ca312b1fe9c2d5403f159d
Guilherme Granato <guilherme.granato@yaman.com.br>
2019-09-16 16:18:51 -0300

    testes 1.0

--- src/test/java/Teste.java
+++ src/test/java/Teste.java
@@ -1,6 +1,20 @@
 package test.java;
 
-public class Teste{
-    
+import main.java.Endereco;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class Teste{
+
+    @Test
+    public void teste01(){
+        Endereco endereco = new Endereco("TEste",12,"TEste2","tese","ree");
+        Assert.assertEquals(12,endereco.getNumero());
+    }
+
+    @Test
+    public void teste02(){
+        Endereco endereco = new Endereco("TEste",12,"TEste2","tese","ree");
+        Assert.assertEquals("Teste",endereco.getLogradouro());
+    }
 }
