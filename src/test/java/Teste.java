27e000fad38284ae3a93c6f878c4f8db5149cacc
Rafael Costa <rafael.costa@yaman.com.br>
2019-09-17 15:29:48 -0300

    Adicionado metodo sacar e depositar

--- src/test/java/Teste.java
+++ src/test/java/Teste.java
@@ -1,4 +1,22 @@
 package test.java;
 
+import main.java.ContaCorrente;
+import main.java.ContaPoupanca;
+import main.java.PF;
+
 public class Teste{
+    public static void main(String[] args) {
+        PF pf1 = new PF("Fernanda", "123456789", "45956862858", "fernada@fernanda","912345678", "Alameda Rio Negro",500,"Alphaville","Barueri","SP");
+        ContaCorrente cc1 = new ContaCorrente();
+        ContaPoupanca cp1 = new ContaPoupanca();
+
+        cp1.criarConta(pf1);
+        cc1.criarConta(pf1);
+
+        cc1.sacarPF(pf1.getCpf(), 200, cp1);
+        System.out.println(cc1.consultarSaldoContaPF(pf1.getCpf()) + cp1.consultarSaldoContaPF(pf1.getCpf()));
+
+        cp1.depositar(1000.00, 30);
+        System.out.println(cc1.consultarSaldoContaPF(pf1.getCpf()) + cp1.consultarSaldoContaPF(pf1.getCpf()));
+    }
 }
